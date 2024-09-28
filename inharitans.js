c099c1a19281eef09b4b474e56afa201b1447b61
kousra <mdkousarmia71@gmail.com>
2024-09-28 08:49:32 +0600

    js practice

---
+++ inharitans.js
@@ -0,0 +1,17 @@
+class vehicle{
+   constructor(nume, price){
+        this.name = name;
+        this.price = price;
+   }
+   move(){
+    console.log('car is not movet');
+    
+   }
+}
+
+class name extends vehicle{
+    constructor(name,price){
+        this.name=name;
+        this.price=price;
+    }
+}
